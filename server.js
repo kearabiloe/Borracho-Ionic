bcb75fc0347a791568ff0a34c1a6d8be39002dcf
kearabiloe Ledwaba <kearabiloe.ledwaba@gmail.com>
2018-10-25 16:21:26 +0200

    Serving Cordova web

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path')
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, "www")));
+app.use(express.static(path.resolve(__dirname, "platforms/browser/www")));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
  console.log("listening to Port", app.get("port"));
